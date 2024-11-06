f288f4035ebe6719b57e4322b0384eb8ae3950e0
Belen <belen.neira.quiroz@gmail.com>
2024-11-06 00:26:19 -0300

    insertar

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//actualizar//
+//shazam//
 namespace tienda
 {
     public partial class Form1 : Form
