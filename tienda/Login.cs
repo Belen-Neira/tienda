8b397ccab994fb2e8b224a29c3efc311823fe006
PABLO REYES ZUNIGA <pab.reyesz@duocuc.cl>
2024-11-07 00:52:04 -0300

    Creación backend login, falta diseño basico y conexión oracle sql

---
+++ tienda/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tienda
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        //CONEXIÓN A LA BASE DE DATOS ORACLE
+        OracleConnection conexion = new OracleConnection("DATA SOURCE = ** ; PASSWORD = ** ; USER ID = **");
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+        
+            conexion.Open();
+            OracleCommand comando = new OracleCommand("SELECT * FROM LIBRERIA_IMAGINA.USUARIO WHERE NOMBRE = :nombre AND PASSWORD = :pass", conexion);
+
+            comando.Parameters.AddWithValue(":nombre", txtNombre.Text);
+            comando.Parameters.AddWithValue(":pass", txtPass.Text);
+
+            OracleDataReader lector = comando.ExecuteReader();
+
+            if(lector.Read())
+            {
+                //SUJETO A CAMBIOS SE LLAMA A LA SIGUIENTE VISTA QUE PASA DEL LOGIN
+                Form1 formulario = new Form1();
+                conexion.Close();
+                formulario.Show();
+            }
+            else
+            {
+                MessageBox.Show("No es posible acceder");
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        { 
+        
+
+        }
+    }
+}
